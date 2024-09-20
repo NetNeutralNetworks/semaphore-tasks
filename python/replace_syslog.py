ebbf818e1d085cfab7708da7eec0b654c3df140b
Piet Braat <piet.braat@ncubed.nl>
2024-09-20 09:50:39 +0200

    ADD: multiprocessing

--- python/replace_syslog.py
+++ python/replace_syslog.py
@@ -10,22 +10,20 @@ import ipaddress
 
 from nc_mis.drivers.hp.procurve import PROCURVE
 from nc_mis.drivers.aruba.aos import AOS
+import concurrent.futures
+import multiprocessing
 
-librenms = LibreNMS()
-netbox = Netbox()
-# get devices from netbox
-# f"https://{os.environ.get('NETBOX_HOST','')}/api/dcim/devices/?device_role=switch&manufacturer=hpe"
-#
+MAX_WORKERS = os.environ.get('MAX_WORKERS',multiprocessing.cpu_count())
 
-lnms_devices = librenms.get_all_devices()
-lnms_devices = {d['sysName'].split('.')[0]:d for d in lnms_devices}
-nb_devices = netbox.get_all_devices()
+def exec_pool(FUNCTION,LIST):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        executor.map(FUNCTION,LIST)
 
-for nb_device in nb_devices:
+def puch_change(nb_device):
     try:
         lnms_device = lnms_devices.get(nb_device.get('display',''))
         if not lnms_device:
-            continue
+            return
         device_manufacturer = nb_device.get('device_type').get('manufacturer').get('slug')
         #device_role = nb_device.get('device_role').get('slug')
         device_os = lnms_device.get('os')
@@ -40,7 +38,7 @@ for nb_device in nb_devices:
                                     )
             except:
                 print (f"{nb_device.get('display')},{device_ip}: Management ip not found or failed to authenticate")
-                continue
+                return
             
             device.conn.establish_connection()
             device.conn.read_until_prompt_or_pattern('Press any key to continue')
@@ -65,7 +63,7 @@ for nb_device in nb_devices:
             if not commands:
                 device.conn.disconnect()
                 print(f"{nb_device.get('display')},{device_ip}: No changes needed")
-                continue
+                return
             
             # send commands to device
             device.conn.config_mode()
@@ -88,16 +86,32 @@ for nb_device in nb_devices:
                                     )
             except:
                 print (f"{nb_device.get('display')},{nb_device.get('primary_ip')}: Management ip not specified in netbox")
-                continue
+                return
             
             print(f"{host}: Config changed and saved")
             
         else:
             if os.environ.get('DEBUG',False):
                 print(f"{nb_device.get('display')}: No manufacturer set or device is not a switch")
-            continue
+            return
     except Exception as e:
         print (f"{e}\n\n{nb_device.get('display')},{nb_device.get('primary_ip')}: Failure in communication, please contact an engineer to look in to the issue, in the mean time check if changes can be done by logging in localy.")
+
+librenms = LibreNMS()
+netbox = Netbox()
+# get devices from netbox
+# f"https://{os.environ.get('NETBOX_HOST','')}/api/dcim/devices/?device_role=switch&manufacturer=hpe"
+#
+
+lnms_devices = librenms.get_all_devices()
+lnms_devices = {d['sysName'].split('.')[0]:d for d in lnms_devices}
+nb_devices = netbox.get_all_devices()
+
+############################
+# push changes
+############################
+exec_pool(puch_change,nb_devices)
+    
     
     
     
