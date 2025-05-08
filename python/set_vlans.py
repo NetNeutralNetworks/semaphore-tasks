bd1f82fe682d09e00cd69d564a6504c3bd3061ba
Jasper Schoenmaker <5970586+Yoyasp@users.noreply.github.com>
2025-05-08 15:38:22 +0000

    add skip for offline devices

--- python/set_vlans.py
+++ python/set_vlans.py
@@ -183,7 +183,7 @@ netbox = Netbox()
 lnms_devices = librenms.get_all_devices()
 netbox_devices = netbox.get_all_devices()
 # filter only devices that are up 
-#lnms_devices = [d for d in lnms_devices if d.get('status') == 1]
+lnms_devices = [d for d in lnms_devices if d.get('status') == 1]
 lnms_devices_map = {d['sysName'].split('.')[0]:d for d in lnms_devices}
 netbox_devices_map = {d['name']:d for d in netbox_devices}
 
